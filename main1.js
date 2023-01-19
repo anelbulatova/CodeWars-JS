262aed87b13b479de8eecdb9914464cc15aa380b
Anelya <anel.bulatova7@gmail.com>
2023-01-19 23:43:27 +0300

    comment

--- main1.js
+++ main1.js
@@ -1,13 +1,13 @@
 function findUniq(arr) {
-  var map={};
-  for(var i=0; i< arr.length;i++){
-    (arr[i] in map)?map[arr[i]]+=1 : map[arr[i]]=1;
+  var map = {};
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] in map ? (map[arr[i]] += 1) : (map[arr[i]] = 1);
   }
-  
-  for(var key in map){
-    if(map[key]==1){
+
+  for (var key in map) {
+    if (map[key] == 1) {
       return parseFloat(key);
     }
+    // 6 kyu 15 points for it
   }
 }
-
