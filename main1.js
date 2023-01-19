0d9d13af34a8b1da13b2977ad385d728ef287ee5
Anelya <anel.bulatova7@gmail.com>
2023-01-19 23:41:16 +0300

    new

---
+++ main1.js
@@ -0,0 +1,13 @@
+function findUniq(arr) {
+  var map={};
+  for(var i=0; i< arr.length;i++){
+    (arr[i] in map)?map[arr[i]]+=1 : map[arr[i]]=1;
+  }
+  
+  for(var key in map){
+    if(map[key]==1){
+      return parseFloat(key);
+    }
+  }
+}
+
